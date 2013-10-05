2bcacfaf2c4c1e5464ec65233cafcfb75431a7c1
Richard Weiss <richardweiss@richardweiss.org>
2013-10-05 17:13:00 +1000

    Joins of tickets to pages, a reader script for tickets.

--- tickets/maketickets.sh
+++ tickets/maketickets.sh
@@ -1,8 +1,45 @@
+#!/bin/bash
 while read p; do
   echo $p
   qrencode -t PNG -l m -o "codes/qrcode$p.out.png" "http://richardweiss.org/chromatic/ticket?ticket=$p"
-  convert -resize 300x300 codes/qrcode$p.out.png codes/qrcode$p.out.png
-  convert -gravity center -append ticket_l.png codes/qrcode$p.out.png tickets/codedticket_$p.out.png
+  convert -negate -resize 490x490 codes/qrcode$p.out.png codes/qrcode$p.out.png
+  convert -gravity center -append ticket.jpg codes/qrcode$p.out.png tickets/codedticket_$p.out.png
 done < ticketnames.txt
 
-convert +append tickets/codedticket_* tickets.png
+FILES=./tickets/*
+i=1;
+j=1;
+listfiles="";
+
+for f in $FILES
+do
+   listfiles="$listfiles $f"
+   if [ $i -eq 5 ]
+   then
+      echo "convert $listfiles +append horizjoins/ticket$j.j1.jpg"
+      convert $listfiles \+append horizjoins/ticket$j.j1.jpg
+      listfiles=""
+      i=0
+      j=$(($j + 1))
+   fi
+   i=$(($i + 1))
+done;
+
+FILES=./horizjoins/*
+i=1;
+j=1;
+listfiles="";
+
+for f in $FILES
+do
+   listfiles="$listfiles $f"
+   if [ $i -eq 3 ]
+   then
+      echo $listfiles
+      convert $listfiles -append vertjoins/ticket$j.j1.jpg
+      listfiles=""
+      i=0
+      j=$(($j + 1))
+   fi
+   i=$(($i + 1))
+done;
